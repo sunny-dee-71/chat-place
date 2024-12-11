8770f55327c4ce116dc2e9b0f81bac39b58ee8d4
sunny-dee-71 <sunnydee148@gmail.com>
2024-12-11 13:26:00 +0000

    fixed auto delet

--- scripts.js
+++ scripts.js
@@ -82,17 +82,17 @@ const displayMessages = (snapshot) => {
     const usernameElement = document.createElement('div');
     
     if (message.username.length > 19){
-      message.remove();
+      message.remove()
       return;
     }
 
     if (message.text.length > 540){
-      message.remove();
+      message.remove()
       return;
     }
     
     if (message.text.length < 2){
-      message.remove();
+      message.remove()
       return;
     }
 
